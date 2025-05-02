0887e4ce60a28f082a0077245321257380948265
ILeshiy <153270138+ILeshiy@users.noreply.github.com>
2025-05-02 02:32:39 -0700

    updt

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRB;
+    private Animator playerAnim;
+    public ParticleSystem explosion;
+    public ParticleSystem dirt;
+
+
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -13,15 +18,18 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround){
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround &&!gameOver){
             playerRB.AddForce(Vector3.up *jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirt.Stop();
         }
     }
 
@@ -29,10 +37,15 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground")){
             isOnGround=true;
+            dirt.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle")){
             gameOver = true;
             Debug.Log("Game over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosion.Play();
+            dirt.Stop();
         }
     }
 }
